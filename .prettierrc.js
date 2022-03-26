59692f1569b343bd8a3d6d949274be3c7e272668
TamTH-Dev <tamth.dev@gmail.com>
2022-03-26 22:44:27 +0700

    Initialize project

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  trailingComma: 'es5',
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+}
---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import styles from './app.module.scss';
+
+function App() {
+  return <div className={styles['app']}>App</div>
+}
+
+export default App
